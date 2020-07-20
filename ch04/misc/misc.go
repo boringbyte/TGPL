1060f24a1d50ee9d41f1e83735cfffa71411c3ed
boringbyte <kottapallisivaram@gmail.com>
2020-07-20 11:53:41 -0400

    started chapter4

---
+++ ch04/misc/misc.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a [3]int
+	fmt.Println(a[0])
+	fmt.Println(a[len(a)-1])
+
+	for i, v := range a {
+		fmt.Printf("%d %d\n", i, v)
+	}
+
+	for _, v := range a {
+		fmt.Printf("%d\n", v)
+	}
+
+	var q [3]int = [3]int{1, 2, 3}
+	var r [3]int = [3]int{1, 2}
+	fmt.Println(r[2])
+	fmt.Println(q)
+
+	p := [...]int{1, 2, 3}
+	fmt.Println(p)
+
+	s := [3]int{1, 2, 3}
+	fmt.Println(s)
+}
